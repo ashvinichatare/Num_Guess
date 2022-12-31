dab1c98b20fbeac6f120f2088ed70d3eaa6546d1
unknown <ashvinichatare@gmail.com>
2022-12-31 23:54:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use script';
+
+// console.log(document.querySelector('.message').textContent); 
+// document.querySelector('.message').textContent='🎉 Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.number') .textContent = 13;
+// document.querySelector('.score').textContent = 20;
+
+ 
+
